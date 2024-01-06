9779f7ed7dc15545bcc736d8456de1e8581353fa
DamienLADEIRA <damienladeira172777@gmail.com>
2024-01-06 15:46:02 +0100

    LesYeuxVeulentpasuivrent
    J'ai envie de canner

--- Assets/Damien/Scripts/RotateEyes.cs
+++ Assets/Damien/Scripts/RotateEyes.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class RotateEyes : MonoBehaviour
 {
+    //public GameObject player;
+    //public GameObject Eye;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
     {
+      
         //object.Transform.LookAt(player.transform.position);
     }
+
 }
